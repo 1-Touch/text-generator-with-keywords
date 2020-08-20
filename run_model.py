e53ecbb2f1cd85d337cc5edc8b491a700c2b056f
Vaibhav <vaibhav@1touch-dev.com>
2020-08-20 18:01:28 +0530

    Added keywords picking functionality

--- run_model.py
+++ run_model.py
@@ -1,12 +1,27 @@
 import gpt_2_simple as gpt2
-from encode_keyword import encode_keywords
+import spacy
 
-         
-words = encode_keywords(csv_path='harry_potter_small.txt')
+data = ''
 
-s = " "
-s = s.join(words)
-print(s)
+with open('harry_potter_small.txt', 'r') as f:
+    data = f.readlines()
+
+nlp = spacy.load("en_core_web_sm")
+
+doc = nlp(data[0])
+
+# for ent in doc.ents:
+#     print(ent.text, ent.label_)
+
+
+x = []
+for i in doc.ents:
+    if i.label_ == "PERSON" or i.label_ == "ORG":
+        x.append(i.text)
+
+keywords = " ".join(x)
+print(keywords)
+
 
 sess = gpt2.start_tf_sess()
 gpt2.load_gpt2(sess)
@@ -16,8 +31,8 @@ gpt2.generate(sess,
               top_k=40,
               nsamples=1,
               batch_size=1,
-              length=100,
-              prefix=f"~^wizard",   #Put keywords with space eg: wizard harry magic
+              length=200,
+              prefix=f"~^{keywords}",   #Put keywords with space eg: wizard harry magic
               truncate="",
               include_prefix=False
               )
